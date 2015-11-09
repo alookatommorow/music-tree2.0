7cff2d5127dadc1a967634ca5db7b4abccc4547e
alookatommorow <hessjohnt@gmail.com>
2015-11-08 22:29:05 -0800

    add ajax request for search form

--- client/components/SearchForm.jsx
+++ client/components/SearchForm.jsx
@@ -1,11 +1,54 @@
 var React = require('react');
 
+
 var SearchForm = React.createClass ({
+  getInitialState: function(){
+    return {
+      formAction: 'http://localhost:3000/search',
+      formMethod: 'get',
+    }
+  },
+
+  executeSearch: function() {
+    console.log("executing search");
+    var data = {
+      query: this.refs.query.value,
+    };
+    console.log(data);
+    $.ajax({
+      url: this.state.formAction,
+      data: data,
+      dataType: 'json',
+      success: this.successFunction,
+      error: this.errorFunction,
+    });
+  },
+
+  handleSubmit: function(event) {
+    event.preventDefault();
+    // var firstName = React.findDOMNode(this.refs.firstName).getValue()
+
+    console.log("hella tite");
+    console.log(this.state.formAction);
+    this.executeSearch();
+  },
+
+  successFunction: function(response){
+    console.log("success");
+    for (var i = 0; i < response.length; i++) {
+      console.log(response[i][type]);
+    }
+  },
+
+  errorFunction: function(){
+    console.log("error");
+  },
+
   render: function () {
     return (
       <div>
-        <form action="http://localhost:3000/search" method="get">
-          <input type="text" name="query" placeholder="Search..." ></input>
+        <form action={this.state.formAction} method={this.state.formMethod} onSubmit={this.handleSubmit}>
+          <input type="text" ref="query" placeholder="Search..." ></input>
           <input type="submit" value="Search"></input>
         </form>
       </div>
