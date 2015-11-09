9e20920980c8a994ef2cebee04df251cd604c169
alookatommorow <hessjohnt@gmail.com>
2015-11-08 19:10:52 -0800

    add home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+class HomeController < ApplicationController
+    skip_before_filter  :verify_authenticity_token
+    def index
+
+    end
+
+    def search
+        p params
+        results = Discogs::Client.new.search(params[:query], params[:specs])
+        render json: results
+    end
+
+    def subsearch
+        p params
+        results = Discogs::Client.new.discog(params[:query])
+        render json: results
+
+    end
+
+end
