52745a2b3f98beacba45b8ea3cf6f7195384e94f
alookatommorow <hessjohnt@gmail.com>
2015-11-20 17:57:29 -0800

    add select options to search form

--- client/components/SearchForm.jsx
+++ client/components/SearchForm.jsx
@@ -3,23 +3,19 @@ var ResultsContainer = require('./ResultsContainer.jsx')
 
 
 var SearchForm = React.createClass ({
-  getInitialState: function () {
-    return {
-      query: "",
-    };
-  },
-
-  handleChange: function() {
-    this.setState({query: event.target.value});
-  },
 
   render: function () {
     return (
       <div>
-
-          <input className="query" type="text" placeholder="Search..." onChange={this.handleChange}></input>
-          <button type="button" onClick={this.props.handleSearch(this.state.query)}>Search</button>
-
+        <form action={this.props.formAction} method={this.props.formMethod} onSubmit={this.props.handleSubmit} >
+          <input className="query" type="text" placeholder="Search..." onChange={this.props.handleChange}></input>
+          <select name="select">
+            <option value="artist" selected>Artist</option>
+            <option value="album">Album</option>
+            <option value="song">Song</option>
+          </select>
+          <input type="submit" value="Search" ></input>
+        </form>
       </div>
     );
   },
