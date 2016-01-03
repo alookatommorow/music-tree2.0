bfa9f5d67dbc6200842e73e6cd2a280523bf267f
alookatommorow <hessjohnt@gmail.com>
2016-01-02 23:45:46 -0800

    add state to album component

--- client/components/Album.jsx
+++ client/components/Album.jsx
@@ -2,8 +2,15 @@ var React = require('react');
 var ListItem = require('material-ui/lib/lists/list-item');
 
 var Album = React.createClass ({
-  render: function () {
+  getInitialState: function(){
+    return {
+      details: null,
+      url: this.props.origin + "/album_info",
+      showCloseButton: false,
+    };
+  },
 
+  render: function () {
     return (
         <div>
           <ListItem>{this.props.albumTitle} {this.props.albumYear}</ListItem>
