1773520d305ecd7fe2d71c87aa88b8f04a2de8bc
alookatommorow <hessjohnt@gmail.com>
2016-01-24 17:41:08 -0800

    add NoResults component

--- client/components/ResultsContainer.jsx
+++ client/components/ResultsContainer.jsx
@@ -1,18 +1,13 @@
 var React = require('react');
 var List = require('material-ui/lib/lists/list');
 var Result = require('./Result.jsx');
+var NoResults = require('./NoResults.jsx');
 
 var ResultsContainer = React.createClass ({
 
   render: function () {
     var searchResults
-    var noResults =
-      <div className="center-text">
-        <img className="no-results-image" src="https://storage.googleapis.com/west-coast-skateparks/no-results.jpg"></img>
-        <div className="no-results">
-          No Results For Your Query
-        </div>
-      </div>
+    var noResults = < NoResults/>
     if (this.props.results.length > 0){
       searchResults = this.props.results.map(function(result, index){
         if (result.type === this.props.queryType) {
