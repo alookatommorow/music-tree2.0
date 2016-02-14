93e0c180f169583a3f329603a4694c4dadb10f4a
alookatommorow <hessjohnt@gmail.com>
2016-02-13 17:49:42 -0800

    refactor discog and discog helper

--- app/models/concerns/discog_helper.rb
+++ app/models/concerns/discog_helper.rb
@@ -1,31 +1,5 @@
 module DiscogHelper
 
-  def format_url(query, is_discog)
-    ## this is workaround for characters like "ö" as in Motörhead being passed in as a query (something about utf vs. ascii )
-    url = URI.parse("/database/search")
-    search_keys = {'q' => "#{query}"}
-    discog_keys = {'type' => "master", 'artist' => "#{query}"}
-    required_keys = {
-        'key' => "#{ENV['CONSUMER_KEY']}",
-        'secret' => "#{ENV['CONSUMER_SECRET']}",
-        'per_page' => 100
-      }
-    if is_discog
-      url.query = URI::encode_www_form(discog_keys.merge(required_keys))
-    else
-      url.query = URI::encode_www_form(search_keys.merge(required_keys))
-    end
-    url.to_s
-  end
-
-  def sort_by_year(results)
-    results.sort_by { |item| item["year"] }
-  end
-
-  def eliminate_nil(results)
-    results.select { |result| result["year"] }
-  end
-
   def search_filter(results)
     filtered_results = { artistResults: [], albumResults: []}.tap do |fr|
       results.each do |result|
@@ -39,6 +13,7 @@ module DiscogHelper
   end
 
   def discography_filter(results)
+    results = sort_by_year(eliminate_nil(results))
     filtered_results = {all: results, eps: [], lps: []}.tap do |fr|
       results.each do |album|
         if (album["format"] & ['Album', 'Compilation']).any?
@@ -47,5 +22,33 @@ module DiscogHelper
           fr[:eps].push(album)
         end
       end
+    end
   end
+
+  private
+
+    def sort_by_year(results)
+      results.sort_by { |item| item["year"] }
+    end
+
+    def eliminate_nil(results)
+      results.select { |result| result["year"] }
+    end
+
+    def search_keys
+      {'q' => "#{query}"}
+    end
+
+    def discog_keys
+      {'type' => "master", 'artist' => "#{query}"}
+    end
+
+    def required_keys
+      {
+        'key' => "#{ENV['CONSUMER_KEY']}",
+        'secret' => "#{ENV['CONSUMER_SECRET']}",
+        'per_page' => 100
+      }
+    end
+
 end
--- app/models/discog.rb
+++ app/models/discog.rb
@@ -3,30 +3,51 @@ module Discog
     include HTTParty
     include DiscogHelper
 
-    base_uri "https://api.discogs.com"
-
-    def search(query)
-      search_filter(parsed_results(query, false))
+    def initialize(query, is_discog = false)
+      @query = query
+      @is_discog = is_discog
     end
 
-    def artist_info(query)
+    base_uri "https://api.discogs.com"
+
+    def search
+      search_filter(parsed_results)
+      # Filter.new.search
+    end
+
+    def artist_info
       self.class.get("/artists/#{query}").parsed_response["profile"]
     end
 
-    def album_info(query)
+    def album_info
       self.class.get("/masters/#{query}").parsed_response
     end
 
-    def discog(query)
-      results = eliminate_nil(parsed_results(query, true))
-      discography_filter(sort_by_year(results))
+    def discog
+      discography_filter(parsed_results)
+      # Filter.new(query).discography
     end
 
     private
 
-    def parsed_results(query, is_discog)
-      self.class.get(format_url(query, is_discog)).parsed_response["results"]
-    end
+      attr_reader :query, :is_discog
+
+      def url
+        ## this is workaround for characters like "ö" as in Motörhead being passed in as a query (something about utf vs. ascii )
+        URI.parse("/database/search").tap {|url| format_url(url).to_s }
+      end
+
+      def format_url(url)
+        if is_discog
+          url.query = URI::encode_www_form(discog_keys.merge(required_keys))
+        else
+          url.query = URI::encode_www_form(search_keys.merge(required_keys))
+        end
+      end
+
+      def parsed_results
+        self.class.get(url).parsed_response["results"]
+      end
 
   end
 end
