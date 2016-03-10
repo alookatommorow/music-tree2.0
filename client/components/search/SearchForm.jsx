17df9b2d7082408b195cf8df28e706574a5c8d58
alookatommorow <hessjohnt@gmail.com>
2016-03-10 11:13:52 -0800

    put search form in row

--- client/components/search/SearchForm.jsx
+++ client/components/search/SearchForm.jsx
@@ -8,7 +8,7 @@ var SelectField = require('material-ui/lib/select-field');
 var MenuItem = require('material-ui/lib/menus/menu-item');
 var TextField = require('material-ui/lib/text-field');
 
-var Col = require('react-bootstrap/lib/Col');
+
 
 injectTapEventPlugin();
 
@@ -20,24 +20,22 @@ var SearchForm = React.createClass ({
       fontFamily: "Muli"
     }
     return (
-      <Col xs={12} md={8} lg={4} lgOffset={4} mdOffset={2} >
-        <div className="search-form" >
-          <form onSubmit={this.props.handleSubmit} >
-            <div>
-              <TextField className="query" inputStyle={queryStyle} onChange={this.props.handleChange} hintText="Enter search..." />
-            </div>
-            <div>
+      <div>
+        <form onSubmit={this.props.handleSubmit} >
+          <div>
+            <TextField className="query" inputStyle={queryStyle} onChange={this.props.handleChange} hintText="Enter search..." />
+          </div>
+          <div>
             <SelectField value={this.props.queryType} onChange={this.props.handleSelect}>
               <MenuItem value={'artist'} primaryText="Artist"/>
               <MenuItem value={'master'} primaryText="Album"/>
             </SelectField>
-            </div>
-            <div className="search-button">
-              <RaisedButton label="Search" secondary={true} labelStyle={this.props.buttonStyle} onClick={this.props.handleSubmit} />
-            </div>
-          </form>
-        </div>
-      </Col>
+          </div>
+          <div className="search-button">
+            <RaisedButton label="Search" secondary={true} labelStyle={this.props.buttonStyle} onClick={this.props.handleSubmit} />
+          </div>
+        </form>
+      </div>
     );
   },
 
