d691eb4e8146b86ae032018418d0e38d2e470479
alookatommorow <hessjohnt@gmail.com>
2016-03-19 00:18:34 -0700

    clean up discogs container

--- client/components/discogs/DiscogContainer.jsx
+++ client/components/discogs/DiscogContainer.jsx
@@ -1,74 +1,30 @@
 var React = require('react');
-
 var AlbumResult = require('../results/AlbumResult.jsx');
-
-var LinearProgress = require('material-ui/lib/linear-progress');
-var RaisedButton = require('material-ui/lib/raised-button');
-var ListItem = require('material-ui/lib/lists/list-item');
+var Button = require('react-bootstrap/lib/Button');
 
 var DiscogContainer = React.createClass({
-  getInitialState: function(){
-    return {
-      showMixed: true,
-      showLps: false,
-      showEps: false,
-    };
-  },
-
-  handleAllClick: function(){
-    this.setState({showEps: false, showLps: false, showMixed: true,})
-  },
-
-  handleLpClick: function(){
-    this.setState({showEps: false, showMixed: false, showLps: true, })
-  },
-
-  handleEpClick: function(){
-    this.setState({showLps: false, showMixed: false, showEps: true})
-  },
 
   render: function(){
-    var generateDiscog = function(collection) {
-      var albums = []
-      collection.map(function(album, index){
-        albums.push(<AlbumResult closeButtonStyle={this.props.closeButtonStyle} buttonStyle={this.props.buttonStyle} key={album.id} ajaxRequest={this.props.ajaxRequest} result={album} origin={this.props.origin} query={this.props.query} />);
+    var albums =
+      this.props.albums.map(function(album, index){
+        return <AlbumResult closeButtonStyle={this.props.closeButtonStyle} buttonStyle={this.props.buttonStyle} key={album.id} ajaxRequest={this.props.ajaxRequest} result={album} origin={this.props.origin} query={this.props.query} />;
       }.bind(this));
-      return albums;
-    }.bind(this);
-
-    var generateButton = function(label, handler){
-      return <RaisedButton labelStyle={this.props.buttonStyle} label={label} onClick={handler}/>;
-    }.bind(this);
 
     var closeButton =
-    <div className="right two-bottom">
-      <RaisedButton labelStyle={this.props.closeButtonStyle} onClick={this.props.handleCloseClick} label="Close" />
-    </div>
+      <div className="right two-bottom">
+        <Button bsStyle="danger" onClick={this.props.handleCloseClick}>CLOSE</Button>
+      </div>
 
-    var allButton = generateButton('All', this.handleAllClick);
-    var lpButton = generateButton('Full Length Albums', this.handleLpClick);
-    var epButton = generateButton('Singes and Other', this.handleEpClick);
     var header = <div className="detail-discog-header">{this.props.title} Discography</div>
-
-    var mixed = generateDiscog(this.props.albums)
-    // var discogLps = generateDiscog(this.props.lps)
-    // var discogEps = generateDiscog(this.props.eps)
 
     var discog =
       <div className="details-display">
         {closeButton}
         <div className="center-text">
           {header}
-          <div className="three-bottom">
-            {allButton}
-            {lpButton}
-            {epButton}
-          </div>
-         </div>
+        </div>
         <div className="clear-right left-text">
-          {this.state.showMixed ? mixed : null}
-          {this.state.showLps ? discogLps : null}
-          {this.state.showEps ? discogEps : null}
+          {albums}
         </div>
       </div>
 
@@ -77,6 +33,7 @@ var DiscogContainer = React.createClass({
         {closeButton}
         <div className="no-results">Discography Unavailable</div>
       </div>
+
     var discogDisplay = (this.props.albums.length > 0) ? discog : noDiscog;
 
     return(
@@ -84,9 +41,7 @@ var DiscogContainer = React.createClass({
         {discogDisplay}
       </div>
     );
-
   },
-
 });
 
 module.exports = DiscogContainer;
