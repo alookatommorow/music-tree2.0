d3000b6b1d7aab4efc88373c3bcf91a7f5c42cc7
alookatommorow <hessjohnt@gmail.com>
2016-03-22 14:53:02 -0700

    render new album details

--- client/components/details/AlbumDetailsContainer.jsx
+++ client/components/details/AlbumDetailsContainer.jsx
@@ -3,9 +3,18 @@ var React = require('react');
 var AlbumDetailsContainer = React.createClass({
   render: function(){
     var info = this.props.albumInfo;
-    var stats = [];
+    // Subgenres
+    var styles = info.styles.map(function(style){
+      return <div key={style}>{style}</div>;
+    });
+    //Formats
+    var formats = info.formats[0].descriptions.map(function(format){
+      return <div key={format}>{format}</div>;
+    });
+    var label = info.labels[0].name;
+    var country = <div>{info.country}</div>
     var header = <div>Tracklist</div>
-    var genre = <div>{info.genres[0]}</div>;
+    var genre = <div>Genre: {info.genres[0]}</div>
 
     var tracklist = info.tracklist.map(function(track, index){
         return <div key={index}>{index+1}. {track['title']}</div>
@@ -18,6 +27,10 @@ var AlbumDetailsContainer = React.createClass({
             {header}
           </div>
             {genre}
+            {label}
+            {country}
+            {formats}
+            {styles}
           <div className="left-text">
             {tracklist}
           </div>
