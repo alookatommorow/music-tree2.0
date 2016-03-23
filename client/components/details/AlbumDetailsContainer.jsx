a22854f481915704710d1fa57d827314cb862ac6
alookatommorow <hessjohnt@gmail.com>
2016-03-22 23:57:29 -0700

    tentative layout for album deets

--- client/components/details/AlbumDetailsContainer.jsx
+++ client/components/details/AlbumDetailsContainer.jsx
@@ -1,41 +1,88 @@
 var React = require('react');
+var Col = require('react-bootstrap/lib/Col');
+var Row = require('react-bootstrap/lib/Row');
+var Panel = require('react-bootstrap/lib/Panel');
+var Well = require('react-bootstrap/lib/Well');
 
 var AlbumDetailsContainer = React.createClass({
   render: function(){
+    var generateHeader = function(text) {
+      return <div className="bold one-em">{text}</div>;
+    }
+    // var generateList = function(collection) {
+    //   collection.map
+    // }
+
     var info = this.props.albumInfo;
-    // Subgenres
+    var genreHeader = generateHeader("Genre:");
+    var labelHeader = generateHeader("Label:");
+    var countryHeader = generateHeader("Country:");
+    var stylesHeader = generateHeader("Styles:");
+    var tracklistHeader = generateHeader("Tracklist:");
+    var formatsHeader = generateHeader("Formats:");
+
+    var label = info.labels[0].name;
+    var country = <div>{info.country}</div>;
+    var genre = info.genres[0];
     var styles = info.styles.map(function(style){
       return <div key={style}>{style}</div>;
     });
-    //Formats
     var formats = info.formats[0].descriptions.map(function(format){
       return <div key={format}>{format}</div>;
     });
-    var label = info.labels[0].name;
-    var country = <div>{info.country}</div>
-    var header = <div>Tracklist</div>
-    var genre = <div>Genre: {info.genres[0]}</div>
-
     var tracklist = info.tracklist.map(function(track, index){
-        return <div key={index}>{index+1}. {track['title']}</div>
-      });
+      return <div key={index}>{index+1}. {track['title']}</div>
+    });
 
     return (
-      <div>
-        <div className="details-display">
+      <Row className="details-display">
+        <Col xs={3} xsOffset={2}>
           <div className="left-text bold one-bottom">
-            {header}
+            {tracklistHeader}
           </div>
-            {genre}
-            {label}
-            {country}
-            {formats}
-            {styles}
           <div className="left-text">
             {tracklist}
           </div>
-        </div>
-      </div>
+        </Col>
+
+        <Col xs={2}>
+          <div className="two-bottom">
+            {genreHeader}
+            <div className="two-left">
+              {genre}
+            </div>
+          </div>
+          <div className="two-bottom">
+            {stylesHeader}
+            <div className="two-left">
+              {styles}
+            </div>
+          </div>
+        </Col>
+        <Col xs={2}>
+        <Well>
+          <div className="two-bottom">
+            {countryHeader}
+            <div className="two-left">
+              {country}
+            </div>
+          </div>
+          <div className="two-bottom">
+            {labelHeader}
+            <div className="two-left">
+              {label}
+            </div>
+          </div>
+          <div className="two-bottom">
+            {formatsHeader}
+            <div className="two-left">
+              {formats}
+            </div>
+          </div>
+          </Well>
+        </Col>
+
+      </Row>
     );
   }
 });
