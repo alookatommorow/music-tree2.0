aedbb4b0fec7db921d94e06413089b05212edf29
alookatommorow <hessjohnt@gmail.com>
2016-04-04 22:36:33 -0700

    add discog component

---
+++ client/components/discogs/Discog.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var AlbumResult = require('../results/AlbumResult.jsx');
+var Button = require('react-bootstrap/lib/Button');
+var Well = require('react-bootstrap/lib/Well');
+
+var Discog = React.createClass({
+  render: function(){
+
+    return(
+      <div>
+
+      </div>
+    );
+  },
+});
+
+module.exports = Discog;
