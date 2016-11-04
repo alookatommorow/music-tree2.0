987f9897b0ebe38f9a4de59555fc2ee1b5b06ad8
alookatommorow <hessjohnt@gmail.com>
2016-11-04 14:52:09 -0700

    update packages

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,7 +5,7 @@ var Col = require('react-bootstrap/lib/Col');
 var Header = React.createClass ({
   render: function () {
     return (
-      <Col xs={6} s={6} md={4} lg={4} xsOffset={3} sOffset={3} lgOffset={4} mdOffset={4} >
+      <Col xs={6} sm={6} md={4} lg={4} xsOffset={3} smOffset={3} lgOffset={4} mdOffset={4} >
         <div className="center-text">
           <Image src="https://storage.googleapis.com/music-tree/music-tree-logo.png" responsive />
         </div>
